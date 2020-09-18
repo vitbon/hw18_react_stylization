99efb1cadd2dd037add1f1209246d152b4e1eef9
Vitalii <vit.bondarchuk@gmail.com>
2020-09-18 15:15:15 +0300

    Updates

--- precache-manifest.580518e50e71f1a850d14e31c50b3b2c.js
+++ precache-manifest.580518e50e71f1a850d14e31c50b3b2c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "46e2e1fd52a0cf7d61939830f11512e7",
+    "revision": "a2c0f72581d276fb1861e8a12e9df5d3",
     "url": "/hw18_react_stylization/index.html"
   },
   {
-    "revision": "bcfae0d9485af58caac7",
+    "revision": "05f9ff25b9f684e8fbb6",
     "url": "/hw18_react_stylization/static/css/main.fceb6c4d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/hw18_react_stylization/static/js/2.2f79b439.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bcfae0d9485af58caac7",
-    "url": "/hw18_react_stylization/static/js/main.dd290cf6.chunk.js"
+    "revision": "05f9ff25b9f684e8fbb6",
+    "url": "/hw18_react_stylization/static/js/main.ff098077.chunk.js"
   },
   {
     "revision": "cbfca83195fe3588cfe1",
